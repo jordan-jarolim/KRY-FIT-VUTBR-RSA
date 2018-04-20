5e995bc6f3c2b38c173b0032162f4198df9094fd
Jordán Jarolím SDE <jordanjarolimsde@Jordans-MacBook-Pro.local>
2018-04-20 18:12:30 +0200

    initial

---
+++ KRY-RSA/KRY-RSA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KRY-RSA
+//
+//  Created by Jordán Jarolím SDE on 20/04/2018.
+//  Copyright © 2018 FIT VUTBR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
